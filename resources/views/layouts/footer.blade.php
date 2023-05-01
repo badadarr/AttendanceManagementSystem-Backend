d6e1aac687378ae22e5759129933cb84ed5b54f2
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2023-05-02 00:20:58 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    © {{date('Y')}} Simple Attendance Management System <span class="d-none d-sm-inline-block"><i
+            class="mdi mdi-heart text-danger"></i> </span>.
+</footer>
